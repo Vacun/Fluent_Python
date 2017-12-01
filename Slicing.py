cbf66b288ca87ac63f5cf2cb37011ae46ed93793
Vacun <vahe.voskerchyan@gmail.com>
2017-12-01 00:47:18 -0500

    sliceing tricks

--- Slicing.py
+++ Slicing.py
@@ -22,9 +22,27 @@ total_price = slice(55, None)
 
 line_items = invoice.split('\n')[2:]
 for item in line_items:
-    print('***   ',item[Description], item[total_price])
+    print('***   ', item[Description], item[total_price])
 
 
 # Assigning to slices
 
 l = list(range(10))
+
+# for mutiable sequances we can graft,excise or otherwise modify slices in place
+
+l[2:5] = [20, 30]  # here we graft: replacing 3 elements of the list by 2 new ones
+
+# deleteing in place can be done as follows
+
+del l[5:7]
+
+# OR del(l[5:7])
+l[3::2] = [11, 22]  # needs to be the same element count
+
+# when the target of assignment is a slice, then right-hand side must be an iterable
+
+l[2:5] = [100]  # even if it is just one item. here we excise.
+
+
+# Using + and * with sequences
